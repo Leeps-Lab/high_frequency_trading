9e5819ce798cd6df06fe5c566bbd4cd29d893536
hademircii <hdemirci@ucsc.edu>
2018-04-28 22:31:33 -0700

    add jump to routing

--- oTree_HFT_CDA/routing.py
+++ oTree_HFT_CDA/routing.py
@@ -1,10 +1,10 @@
 from channels.routing import route_class
-from .consumers import SubjectConsumer, InvestorConsumer
+from .consumers import SubjectConsumer, InvestorConsumer, JumpConsumer
 from otree.channels.routing import channel_routing
-
 
 
 channel_routing += [
     route_class(SubjectConsumer, path=r"^/hft/(?P<group_id>\w+)/(?P<player_id>\w+)/"),
-    route_class(InvestorConsumer, path=r"^/hft_investor/(?P<group_id>\w+)/")
+    route_class(InvestorConsumer, path=r"^/hft_investor/(?P<group_id>\w+)/"),
+    route_class(JumpConsumer, path=r"^/hft_jump/(?P<group_id>\w+)/")
 ]
