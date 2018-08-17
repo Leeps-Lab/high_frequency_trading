aad0ed3ab2b6c081d79851a45c5e66282bd5f38b
hademircii <hdemirci@ucsc.edu>
2018-08-17 06:54:52 -0700

    single buffer in twisted client

--- hft_bcs/exchange.py
+++ hft_bcs/exchange.py
@@ -6,59 +6,52 @@ from twisted.internet.protocol import Protocol, ClientFactory
 import numpy as np
 from twisted.internet import reactor
 from .hft_logging import session_events as hfl
+from collections import deque
 
 log.startLogging(sys.stdout)
+
 class OUCH(Protocol):
+    bytes_needed = {
+        'S': 10,
+        'E': 40,
+        'C': 28,
+        'U': 80,
+        'A': 66
+    }
 
     def __init__(self):
         super()
-        self.bytes_needed = []
-        self.buffers = []
+        self.buffer = deque()
 
     def connectionMade(self):
-        log.msg("Connection made")
+        log.msg('Connection made.')
       
     def dataReceived(self, data):
-        # TODO: get rid of else ifs.
-        # use some dispatch.
-        # just dont do this ever.
-        if data[0] == ord('S'):
-            self.bytes_needed.append(10)
-        elif data[0] == ord('E'):
-            self.bytes_needed.append(40)
-        elif data[0] == ord('C'):
-            self.bytes_needed.append(28)
-        elif data[0] == ord('U'):
-            self.bytes_needed.append(80)
-        elif data[0] == ord('A'):
-            self.bytes_needed.append(66)
-        else:
-            raise ValueError('Invalid message header {}: {}'.format(chr(data[0]), data))
-            
-        if len(data) >= self.bytes_needed[0]:
-            # TODO: fix number of buffers.
-            self.buffers.append([])
-            remainder = self.bytes_needed.pop(0)
-            self.buffers[0].extend(data[:remainder])
-            buf = self.buffers.pop(0)
+        cls = self.__class__
+        header = chr(data[0])
+        try:
+            bytes_needed = cls.bytes_needed[header]
+        except KeyError:
+             raise ValueError('Unknown header %s.' % header)
+
+        if len(data) >= bytes_needed:
+            remainder = bytes_needed
+            self.buffer.extend(data[:remainder])
             data = data[remainder:]
             try:
-                self.factory.group.receive_from_exchange(bytes(buf))
+                self.factory.group.receive_from_exchange(bytes(self.buffer))
+                self.buffer.clear()
             except AttributeError as e:
                 log.msg(e)
 
         if len(data):
-            self.buffers.append([])
             self.dataReceived(data)
-        #     # self.bytes_needed[0] -= len(data)
-            # self.buffers[0].push(data)
 
     def sendMessage(self, msg, delay):
         msg = msg.tobytes()
-        # can receive a message back (accepted)
-        # can receive 2 messages (accepted, executed)
-        # can receive 0 (replace dying silently)
-        self.buffers.extend(([], []))
+        # can receive a message back (accepted),
+        # can receive 2 messages (accepted, executed),
+        # can receive 0 message (replace dying silently).
         reactor.callLater(delay, self.transport.write, msg)
 
 
@@ -81,6 +74,7 @@ class OUCHConnectionFactory(ClientFactory):
         l = 'lost connection to exchange at %s: %s' % (self.addr, reason)
         log.msg(l)
         hfl.events.push(hfl.exchange, **{'context': l})
+        connector.connect()
 
     def clientConnectionFailed(self, connector, reason):
         log.msg('failed to connect to exchange at %s: %s' % (self.addr, reason))
