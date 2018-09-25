91e05bf1b9216166af19e9c9c14b5e4db0b1bd34
Eric M. Aldrich <ealdrich@gmail.com>
2018-09-25 11:05:53 +0200

    More changes to config generation script.

--- session_config/createMarketEvents.py
+++ session_config/createMarketEvents.py
@@ -3,57 +3,50 @@ import pandas as pd
 import pdb
 import os
 
-def createMarketEvents(nGroups,nPeriods,experimentLength,dateStr,lambdaJVec,lambdaIVec,startPrice,sigJump):
-  
-  # Output file
-  #filePath = "/Users/ealdrich/Dropbox/Academics/Research/UCSC/oTree_HFT_CDA/config/"+dateStr+"/"
-  filePath = dateStr+"/"
+def createMarketEvents(rootPath,investorFile,jumpFile,periodLength,filePath,lambdaJ,lambdaI,startPrice,sigJump):
 
-  # Parameters for jump distribution
-  startPrice = startPrice
-  muJump = 0
-  sigJump = sigJump
-
-  for ix,periodVal in enumerate(range(1,nPeriods+1)):
+    # Parameters for jump distribution
+    startPrice = startPrice
+    muJump = 0
+    sigJump = sigJump
     
     # Parameters for jump times
-    lambdaJ = lambdaJVec[ix]
-    nSimJ = int(2*experimentLength*lambdaJ)
+    nSimJ = int(2*periodLength*lambdaJ)
   
     # Investor parameters
-    lambdaI = lambdaIVec[ix]
-    nSimI = int(2*experimentLength*lambdaI)
+    nSimI = int(2*periodLength*lambdaI)
     buyProb = 0.5
   
-    for groupVal in range(1,nGroups+1):
+    # Simulate times and sizes
+    jumpTimes = np.cumsum(np.around(np.random.exponential(1/lambdaJ,nSimJ)))
+    jumpTimes = np.hstack((jumpTimes[jumpTimes < periodLength], jumpTimes[jumpTimes >= periodLength][0]))
+    jumpTimes = np.around(jumpTimes/1000.0,3)
+    nJump = len(jumpTimes)
+    jumpSizes = startPrice + np.cumsum(np.random.normal(muJump,sigJump,nJump))
+    jumpSizes = (10000*jumpSizes).astype(int)
 
-      # Simulate times and sizes
-      jumpTimes = np.cumsum(np.around(np.random.exponential(1/lambdaJ,nSimJ)))
-      jumpTimes = np.hstack((jumpTimes[jumpTimes < experimentLength], jumpTimes[jumpTimes >= experimentLength][0]))
-      jumpTimes = np.around(jumpTimes/1000.0,3)
-      nJump = len(jumpTimes)
-      jumpSizes = startPrice + np.cumsum(np.random.normal(muJump,sigJump,nJump))
-      jumpSizes = (10000*jumpSizes).astype(int)
+    # Save jumps to CSV
+    jumpData = pd.DataFrame({'time':jumpTimes,'size':jumpSizes},columns=['time','size'])
+    if os.path.isfile(rootPath+'/'+jumpFile):
+        raise OSError('Attempted to create a file that already exists.')
+    if os.path.isdir(os.path.dirname(rootPath+'/'+jumpFile))==False:
+        os.makedirs(os.path.dirname(rootPath+'/'+jumpFile))
+    jumpData.to_csv(rootPath+'/'+jumpFile,index=False)
 
-      # Save jumps to CSV
-      jumpData = pd.DataFrame({'time':jumpTimes,'size':jumpSizes},columns=['time','size'])
-      filePathSub = filePath+"Period"+str(periodVal)+"/"
-      if os.path.isdir(filePathSub)==False:
-        os.mkdir(filePathSub)
-      jumpFile = filePathSub+"jumps_period"+str(periodVal)+"_group"+str(groupVal)+".csv"
-      jumpData.to_csv(jumpFile,index=False)
+    # Simulate investor arrivals and directions
+    investorTimes = np.cumsum(np.around(np.random.exponential(1/lambdaI,nSimI)))
+    investorTimes = np.hstack((investorTimes[investorTimes < periodLength], investorTimes[investorTimes >= periodLength][0]))
+    investorTimes = np.around(investorTimes/1000.0,3)
+    nInvestor = len(investorTimes)
+    investorDirections = np.random.binomial(1,buyProb,nInvestor)
+    investorDirections = investorDirections.astype(str)
+    investorDirections[investorDirections=='1'] = 'B'
+    investorDirections[investorDirections=='0'] = 'S'
 
-      # Simulate investor arrivals and directions
-      investorTimes = np.cumsum(np.around(np.random.exponential(1/lambdaI,nSimI)))
-      investorTimes = np.hstack((investorTimes[investorTimes < experimentLength], investorTimes[investorTimes >= experimentLength][0]))
-      investorTimes = np.around(investorTimes/1000.0,3)
-      nInvestor = len(investorTimes)
-      investorDirections = np.random.binomial(1,buyProb,nInvestor)
-      investorDirections = investorDirections.astype(str)
-      investorDirections[investorDirections=='1'] = 'B'
-      investorDirections[investorDirections=='0'] = 'S'
-
-      # Save investor arrivals to CSV
-      investorData = pd.DataFrame({'time':investorTimes,'direction':investorDirections},columns=['time','direction'])
-      investorFile = filePath+"Period"+str(periodVal)+"/investors_period"+str(periodVal)+"_group"+str(groupVal)+".csv"
-      investorData.to_csv(investorFile,index=False)
+    # Save investor arrivals to CSV
+    investorData = pd.DataFrame({'time':investorTimes,'direction':investorDirections},columns=['time','direction'])
+    if os.path.isfile(rootPath+'/'+investorFile):
+        raise OSError('Attempted to create a file that already exists.')
+    if os.path.isdir(os.path.dirname(rootPath+'/'+investorFile))==False:
+        os.makedirs(os.path.dirname(rootPath+'/'+investorFile))
+    investorData.to_csv(rootPath+'/'+investorFile,index=False)
--- session_config/createOTreeConfigs.py
+++ session_config/createOTreeConfigs.py
@@ -5,31 +5,78 @@ import pdb
 import yaml
 from createMarketEvents import *
 
-nGroups = int(sys.argv[1])
-nPlayersPerGroup = int(sys.argv[2])
-nPeriods = int(sys.argv[3])
-experimentLengthSeconds = int(sys.argv[4])
-experimentLengthMS = experimentLengthSeconds*1000
-dateStr = sys.argv[5]
-exchangeType = "cda"
-subject = "default"
-startingWealth = 20
-startingPrice = 100
-sigJump = 0.5
-speedCostList = [0.022]*12
-lambdaJVec = [1/3000.0]*12
-lambdaIVec = [1/4000.0]*12
+# Session variables
+nGroups = 3 #int(sys.argv[1])
+nPeriods = 8 #int(sys.argv[3])
+periodLengthSeconds = 240 #int(sys.argv[4])
+trialLengthSeconds = 90 #int(sys.argv[5])
+participationFee = 4
+currency = 'EUR'
+exchangeRate = 2
+randomRoundPayment = False
+sessionKey = 'ColognePilot'
+
+# Configuration variables
+differentDrawsForGroups = True
+createDraws = False
+filePath = 'Pilot' #sys.argv[6]+"/"
+configURLRoot = "https://raw.githubusercontent.com/Leeps-Lab/oTree_HFT_CDA/master/session_config/"
+exchangeURI = "localhost"
+rootPath = os.getcwd()+'/'+filePath
+
+# Environment variables
 maxSpread = 2
 initialSpread = maxSpread/2
-exchangeRate = 2
-filePath = dateStr+"/"
-if os.path.isdir(filePath)==False:
-    os.mkdir(filePath)
-configURLRoot = "https://raw.githubusercontent.com/Leeps-Lab/oTree_HFT_CDA/master/session_config/"+dateStr+"/"
-exchangeURI = "exchanges" #"54.219.182.118"
+startingWealth = 20
 
-# Generate jump and investor files
-createMarketEvents(nGroups,nPeriods,experimentLengthMS,dateStr,lambdaJVec,lambdaIVec,startingPrice,sigJump)
+# Economic variables
+nPlayersPerGroup = 6 #int(sys.argv[2])
+exchangeType = "CDA"
+startingPrice = 100
+sigJump = 0.5
+speedCost = 0.022
+lambdaJ = 1/3.0
+lambdaI = 1/4.0
+
+# Determine if there will be a trial period
+if trialLengthSeconds==0:
+    trialFlag = 0
+else:
+    trialFlag = 1
+    
+# Create file names for investors and jumps
+investorFiles = list()
+jumpFiles = list()
+for group in range(1,nGroups+1):
+    investorFilesGroup = list()
+    jumpFilesGroup = list()
+    if differentDrawsForGroups:
+        draw = group
+    else:
+        draw = 1
+    for period in range(1,nPeriods+1):
+        investorFilesGroup.append("Draw"+str(draw)+"/investors_period"+str(period)+".csv")
+        jumpFilesGroup.append("Draw"+str(draw)+"/jumps_period"+str(period)+".csv")
+    investorFiles.append(investorFilesGroup)
+    jumpFiles.append(jumpFilesGroup)
+    
+# Either create files or check that they exist
+if differentDrawsForGroups:
+    nDraws = nGroups
+else:
+    nDraws = 1
+for draw in range(nDraws):
+    investorFilesForGroup = investorFiles[draw]
+    jumpFilesForGroup = jumpFiles[draw]
+    for period in range(nPeriods):
+        if createDraws == True:
+            createMarketEvents(rootPath,investorFilesForGroup[period],jumpFilesForGroup[period],
+                               periodLengthSeconds,filePath,lambdaJ,lambdaI,startingPrice,sigJump)
+        else:
+            #pdb.set_trace()
+            if ((os.path.isfile(rootPath+'/'+jumpFilesForGroup[period])==False)| 
+                (os.path.isfile(rootPath+'/'+investorFilesForGroup[period])==False)):
+                raise OSError('File does not exist.')
 
 # Create random group assignments
 nPlayers = nGroups*nPlayersPerGroup
@@ -41,27 +88,21 @@ for group in range(nGroups):
 # Create the yaml file
 marketDict = {'matching-engine-host': str(exchangeURI),'design':exchangeType.upper()}
 groupDict = {'number-of-groups':nGroups,'players-per-group':nPlayersPerGroup,'group-assignments':str(groupList)}
+trialDict = {'run':trialFlag,'trial-Length':trialLengthSeconds}
 parametersDict = {'fundamental-price':startingPrice,'max-spread':maxSpread,'initial-spread':initialSpread,
-                  'initial-endowment':startingWealth,'speed-cost':speedCostList[0],'session-length':experimentLengthSeconds}
-demoDict = {'number-of-participants':nGroups*nPlayersPerGroup}
+                  'initial-endowment':startingWealth,'speed-cost':speedCost,'session-length':periodLengthSeconds}
+demoDict = {'number-of-participants':'nan'}
+directoryDict = {'folder':filePath}
+sessionDict = {'session-name': sessionKey,'display-name':'CDA Production',
+               'num-rounds':nPeriods,'currency':currency,'exchange-rate':exchangeRate,
+               'random-round-payment':randomRoundPayment,'participation-fee':participationFee}
 investorsDict = {}
 jumpsDict = {}
-directoryDict = {}
-for ix,period in enumerate(range(1,nPeriods+1)):
-    sessionDict = {'session_name': dateStr+'_'+exchangeType+'_period_'+str(period),'display_name':'CDA Production'}
-    speedCost = speedCostList[ix]
-    parametersDict['speed-cost'] = speedCost
-    directoryDict['folder']  = filePath+'Period'+str(period)+'/'
-    if os.path.isdir(directoryDict['folder'])==False:
-        os.mkdir(directoryDict['folder'])
-    for group in range(1,nGroups+1):
-        marketEventsURL = configURLRoot+"Period"+str(period)+"/investors_period"+str(period)+"_group"+str(group)+".csv"
-        priceChangesURL = configURLRoot+"Period"+str(period)+"/jumps_period"+str(period)+"_group"+str(group)+".csv"
-        investorsDict['group_'+str(group)] = marketEventsURL
-        jumpsDict['group_'+str(group)] = priceChangesURL
-    outputDict = {'session':sessionDict, 'market':marketDict,'group':groupDict, 'investors':investorsDict,
-                  'jumps':jumpsDict, 'parameters':parametersDict,'directory':directoryDict, 'demo':demoDict}
-    fileName = directoryDict['folder']+dateStr+'_'+exchangeType+'_'+str(experimentLengthSeconds)+'s_'+str(nGroups)+'groups_'+str(nPlayersPerGroup)+'players_period'+str(period)+'.yaml'
-    with open(fileName, 'w') as outfile:
-        yaml.dump(outputDict, outfile, default_flow_style=False)
-        os.system('cp '+fileName+' session_configs/')
+for group in range(nGroups):
+    investorsDict['group_'+str(group+1)] = investorFiles[group]
+    jumpsDict['group_'+str(group+1)] = jumpFiles[group]
+outputDict = {'session':sessionDict, 'market':marketDict,'group':groupDict, 'investors':investorsDict,
+              'jumps':jumpsDict, 'parameters':parametersDict,'directory':directoryDict, 'demo':demoDict}
+fileName = 'session_configs/'+exchangeType+'_'+str(nGroups)+'groups_'+str(nPlayersPerGroup)+'players.yaml'
+with open(fileName, 'w') as outfile:
+    yaml.dump(outputDict, outfile, default_flow_style=False)
