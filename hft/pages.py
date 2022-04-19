4fb8e862e920dcbd856db141c6594e5d71b194f3
Marco Gutierrez <a20141676@pucp.pe>
2022-04-19 04:19:30 -0500

    added base code for hft survey

--- hft/pages.py
+++ hft/pages.py
@@ -9,6 +9,7 @@ from .output import TraderRecord
 from .session_results import elo_player_summary, state_for_results_template
 from .utility import ensure_results_ready
 from settings import test_inputs_dir
+from .models import Constants, get_correct_answers
 import random
 
 log = logging.getLogger(__name__)
@@ -50,6 +51,73 @@ class Instructions(Page):
 
 
 # TODO: add here survey page classes
+class General(Page):
+    form_model = 'player'
+    # question fields and whether the participant chose the right answer at first
+    lista=list(Constants.q_and_a_sections["general"].keys()) + [question
+                  + "_right_first" for question in 
+                  Constants.q_and_a_sections["general"].keys()]
+    form_fields = lista
+
+    def vars_for_template(self):
+        return get_correct_answers(Constants.q_and_a_sections, "general")
+
+
+class Inventory(Page):
+    form_model = 'player'
+    # question fields and whether the participant chose the right answer at first
+    form_fields = list(Constants.q_and_a_sections["inventory"].keys()) + [question
+                  + "_right_first" for question in 
+                  Constants.q_and_a_sections["inventory"].keys()]
+
+    def vars_for_template(self):
+        return get_correct_answers(Constants.q_and_a_sections, "inventory")
+
+
+class ExternalMarket(Page):
+    form_model = 'player'
+    # question fields and whether the participant chose the right answer at first
+    form_fields = list(Constants.q_and_a_sections["external_market"].keys()) + [question
+                  + "_right_first" for question in 
+                  Constants.q_and_a_sections["external_market"].keys()]
+
+    def vars_for_template(self):
+        return get_correct_answers(Constants.q_and_a_sections, "external_market")
+
+
+class Speed(Page):
+    form_model = 'player'
+    # question fields and whether the participant chose the right answer at first
+    form_fields = list(Constants.q_and_a_sections["speed"].keys()) + [question
+                  + "_right_first" for question in 
+                  Constants.q_and_a_sections["speed"].keys()]
+
+    def vars_for_template(self):
+        return get_correct_answers(Constants.q_and_a_sections, "speed")
+
+
+class MarketSpecific(Page):
+    form_model = 'player'
+    # question fields and whether the participant chose the right answer at first
+    form_fields = list(Constants.q_and_a_sections["market_specific_design"].keys()) + [question
+                  + "_right_first" for question in 
+                  Constants.q_and_a_sections["market_specific_design"].keys()]
+
+    def vars_for_template(self):
+        correct_answers_dicts = get_correct_answers(Constants.q_and_a_sections, "market_specific_design")
+        correct_answers = {} # placeholder for storing correct answers
+        for question in correct_answers_dicts.keys():
+            # setting correct answer per question
+            if question == 'one_ask':
+                # TODO: validate correct execution of auction_format snippet
+                auction_format = self.session.config['auction_format'].lower()
+                correct_answers[question] = correct_answers_dicts[question][auction_format]
+            else:
+                correct_answers[question] = correct_answers_dicts[question]
+        
+        return correct_answers
+
+###########################
 
 
 class InitialDecisionSelection(Page):
