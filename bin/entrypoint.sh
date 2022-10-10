51399f0411bf5c2c1082ccfec776db5778f4deba
hademircii <hasanalidemirci1@gmail.com>
2022-10-09 18:53:18 -0700

    entrypoint changes.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,7 +2,17 @@
 
 set -ex
 
-yes | otree resetdb
-echo 'yes' | otree collectstatic
+if [ ! -f ~/.hasrun ]; then
+    echo 'reset db..'
+    yes | otree resetdb
+
+    echo 'read configuration files..'
+    python manage.py collect_exg_config
+
+    echo 'collect static files..'
+    yes | otree collectstatic
+
+    touch ~/.hasrun
+fi
 
 otree runhftserver 0.0.0.0:8000
