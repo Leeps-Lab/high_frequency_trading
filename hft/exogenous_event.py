a43d6d397e3621edf6dda3f2f71bc1b205880b95
mirkiux <mirkiux@github.com>
2023-08-07 14:22:23 -0500

    ExogenousEventFile withouth loading fields

--- hft/exogenous_event.py
+++ hft/exogenous_event.py
@@ -3,7 +3,7 @@ import csv
 import logging
 import secrets
 from .models import Model,ForeignKey
-from otree.api import models
+#from otree.api import models
 #from django.db.models import  CharField, CASCADE, FloatField, IntegerField
 
 
@@ -32,12 +32,14 @@ class ExogenousEventModelFactory:
 class ExogenousEventFile(Model):
     #upload_time = models.DateTimeField(auto_now_add=True)   COMMENT
     #upload_name = models.StringField()
-    upload_name = models.CharField(max_length=255)
-    code = models.CharField(primary_key=True, editable=False, null=False,default=secrets.token_hex(8)) #delete default=random_chars_8
+    #upload_name = models.CharField(max_length=255)
+    #code = models.CharField(primary_key=True, editable=False, null=False,default=secrets.token_hex(8)) #delete default=random_chars_8
     #record_type = models.StringField()
-    record_type = models.CharField(max_length=255)
+    #record_type = models.CharField(max_length=255)
     def __str__(self):
-        return self.upload_name
+        pass
+        #return self.upload_name
+    
 
 
 class CSVRowMixIn:
