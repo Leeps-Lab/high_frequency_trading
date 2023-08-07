f61852f82ce22da6cf583662b43193813d240b50
mirkiux <mirkiux@github.com>
2023-08-07 14:16:29 -0500

    CharField import updated to new otree version

--- hft/exogenous_event.py
+++ hft/exogenous_event.py
@@ -3,7 +3,7 @@ import csv
 import logging
 import secrets
 from .models import Model,ForeignKey
-from otree.api import CharField, CASCADE, FloatField, IntegerField
+from otree.api.models import CharField, FloatField, IntegerField
 #from django.db.models import  CharField, CASCADE, FloatField, IntegerField
 
 
@@ -31,10 +31,10 @@ class ExogenousEventModelFactory:
 
 class ExogenousEventFile(Model):
     #upload_time = models.DateTimeField(auto_now_add=True)   COMMENT
-    upload_name = models.StringField()
-    # upload_name = CharField(max_length=255)
+    #upload_name = models.StringField()
+    upload_name = CharField(max_length=255)
     code = CharField(primary_key=True, editable=False, null=False,default=secrets.token_hex(8)) #delete default=random_chars_8
-    # record_type = models.StringField()
+    #record_type = models.StringField()
     record_type = CharField(max_length=255)
     def __str__(self):
         return self.upload_name
