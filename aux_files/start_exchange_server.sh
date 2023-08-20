417030f782e17b3794f8e2b52b10c5822faa4168
mirkiux <mirkiux@github.com>
2023-08-20 18:34:26 -0500

    improved service aux files

--- aux_files/start_exchange_server.sh
+++ aux_files/start_exchange_server.sh
@@ -6,18 +6,15 @@ if [[ ":$PATH:" != *":$PYENV_ROOT/bin:"* ]]; then
     export PATH="$PYENV_ROOT/bin:$PATH"
 fi
 
-#export PATH="$PYENV_ROOT/shims:$PATH
 
 eval "$(pyenv init -)"
 
 #source /home/hft_dev/.pyenv/bin/pyenv
 #/home/hft_dev/.pyenv/bin/pyenv activate pyenv_exchange_server
-pyenv activate 3.6.15/envs/pyenv_exchange_server_two
+pyenv activate 3.6.15/envs/pyenv_exchange_server
 
 # Run your high-frequency trading command
 cd /home/hft_dev/exchange_server
-
-#EXCHANGE_EXECUTABLE
 
 #$EXCHANGE_EXECUTABLE
 python3 run_exchange_server.py --host 0.0.0.0 --port 9001 --debug --mechanism cda
--- aux_files/start_hft.sh
+++ aux_files/start_hft.sh
@@ -2,9 +2,12 @@
 
 # Activate the pyenv environment
 export PYENV_ROOT="/home/hft_dev/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+if [[ ":$PATH:" != *":$PYENV_ROOT/bin:"* ]]; then
+    export PATH="$PYENV_ROOT/bin:$PATH"
+fi
 
-eval "$(pyenv init --path)"
+
+eval "$(pyenv init -)"
 
 #source /home/hft_dev/.pyenv/bin/pyenv
 #/home/hft_dev/.pyenv/bin/pyenv activate pyenv_exchange_server
@@ -14,12 +17,7 @@ pyenv activate 3.6.15/envs/pyenv_exchange_server
 cd /home/hft_dev/high_frequency_trading
 
 export OTREE_EXECUTABLE="/home/hft_dev/.pyenv/versions/pyenv_exchange_server/bin/otree"
-#$OTREE_EXECUTABLE resetdb
-#$OTREE_EXECUTABLE makemigrations
-#$OTREE_EXECUTABLE migrate
-
 python3 manage.py collect_exg_config
-
 $OTREE_EXECUTABLE devserver 0.0.0.0:8000
 
 echo "Script finished"
